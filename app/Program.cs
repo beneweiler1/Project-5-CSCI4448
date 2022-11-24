4b3464a7bd4c95a3016e528eef4c539c709cb309
scfitze <scfitze@gmail.com>
2022-11-24 15:07:05 -0700

    changes FFT values to double

--- app/Program.cs
+++ app/Program.cs
@@ -1,29 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-ShapeFactory factory = new ShapeFactory();
+// ShapeFactory factory = new ShapeFactory();
 
- Shape A = factory.FactoryMethod("A");
- A.draw(10);
+//  Shape A = factory.FactoryMethod("A");
+//  A.draw(10);
 
- Shape B = factory.FactoryMethod("B");
- B.draw(20);
+//  Shape B = factory.FactoryMethod("B");
+//  B.draw(20);
 
 // void setStrategy(IAnimation strat)
 // {
 //     _animation = strat;
 // }
 
-  var context = new Context();
+  // var context = new Context();
 
-  context.setStrategy(new BackSpin());
-  Console.WriteLine(context.executeStrategy(10));
+  // context.setStrategy(new BackSpin());
+  // Console.WriteLine(context.executeStrategy(10));
 
 
-  var color = new ConcreteColor();
-  RedColor red = new RedColor(color);
-  Console.WriteLine(red.Assemble());
+  // var color = new ConcreteColor();
+  // RedColor red = new RedColor(color);
+  // Console.WriteLine(red.Assemble());
   //BlueColor blue = new BlueColor(red);
 
  //top.CallAnimation(10);
 
  //Console.ReadKey();
+
+ FFTmodel model = new FFTmodel();
+ model.readExcelfile("Test");
