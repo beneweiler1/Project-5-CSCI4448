cb37c52d955c586057c3f4b8bafa6e60364e83a3
beneweiler1 <beneweiler1@gmail.com>
2022-12-04 17:00:00 -0700

    git ignore

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
+=======
+// ShapeFactory factory = new ShapeFactory();
+
+//  Shape A = factory.FactoryMethod("A");
+//  A.draw(10);
+>>>>>>> db3b67a0980b256e1407681d8a02f1cdcbf2815a
 
 //  Shape B = factory.FactoryMethod("B");
 //  B.draw(20);
@@ -19,18 +26,34 @@ shape.makeShape(10);
 // Console.WriteLine(A.getColor());
 // Console.WriteLine(A.getAnimation(10));
 
+<<<<<<< HEAD
 //  var animationRenderer = new AnimationRenderer();
 
+=======
+  // var context = new Context();
+
+  // context.setStrategy(new BackSpin());
+  // Console.WriteLine(context.executeStrategy(10));
+>>>>>>> db3b67a0980b256e1407681d8a02f1cdcbf2815a
 
 //  IAnimation spin = new BackSpin();
 //   animationRenderer.setStrategy(spin);
 //   Console.WriteLine(animationRenderer.executeStrategy(10));
 
+<<<<<<< HEAD
 //   var color = new ConcreteColor();
 //   RedColor red = new RedColor(color);
   //Console.WriteLine(red.Assemble());
+=======
+  // var color = new ConcreteColor();
+  // RedColor red = new RedColor(color);
+  // Console.WriteLine(red.Assemble());
+>>>>>>> db3b67a0980b256e1407681d8a02f1cdcbf2815a
   //BlueColor blue = new BlueColor(red);
 
  //top.CallAnimation(10);
 
  //Console.ReadKey();
+
+ FFTmodel model = new FFTmodel();
+ model.readExcelfile("Test");
--- app/src/MVC/FFTview.cs
+++ app/src/MVC/FFTview.cs
@@ -8,11 +8,18 @@ public class FFTview : FFTObserver {
         model.registerObserver(this);
     }
     void updateFFT() {
+<<<<<<< HEAD
         int low = model.getLow();
         int high = model.getHigh();
         int mid = model.getMid();
         Console.WriteLine(low + "," + high + "," + mid);
         //interpreter
+=======
+        double low = model.getLow();
+        double high = model.getHigh();
+        double mid = model.getMid();
+        Console.WriteLine(low + "," + mid + "," + high);
+>>>>>>> db3b67a0980b256e1407681d8a02f1cdcbf2815a
     }
 
 }
